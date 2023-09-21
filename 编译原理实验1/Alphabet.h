56aa104480da93b54b07b7af674ae486967fd93e
Pleiades_Surface <821356557@qq.com>
2023-09-21 10:09:19 +0800

    Add ReadKeywords Function.

--- 编译原理实验1/Alphabet.h
+++ 编译原理实验1/Alphabet.h
@@ -5,11 +5,13 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 enum class TokenType
 {
 	Keyword,  // 关键字
-	Var, String, Number,   // 变量（标识符），字符串，数字
+	Var, String, Char, Number,  // 变量（标识符），字符串，字符，数字
 	Arithmetic, Logic, Relation, Bitwise, Assignment, Others,  // 算术、逻辑、关系、位运算、赋值、杂项运算符
 	Comment,  // 注释
 	Division,  // 分割符
@@ -42,6 +44,7 @@ public:
 	void PrintToken();  // 打印分割后的字符串
 private:
 	std::set<std::string> keywords;  // 保留的关键字
+	void ReadKeywords();  // 从文件中读取关键字到SET中
 	bool judgeComplex(std::string, int pos);  // 判断是否为复合运算符
 	JudgingComplex judgePlus(std::string, int pos);  // 加号是否复合，是的话返回复合运算符
 	JudgingComplex judgeMinus(std::string, int pos);  // 减号是否复合，是的话返回复合运算符
